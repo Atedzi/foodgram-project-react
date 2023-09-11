005a490fc7d2309e9c8549c18604c1b49ab1bf23
Сергей Квашин <kvashinsa@mail.ru>
2023-09-11 20:44:56 +0300

    nginxconf

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,6 @@
+from api import views
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-
-from api import views
 
 app_name = 'api'
 
@@ -13,5 +12,6 @@ router.register('recipes', views.RecipeViewSet, basename='recipes')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
