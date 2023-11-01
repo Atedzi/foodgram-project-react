4f4ed3131fd9a6d4e30137d75b8c7c635fedae54
Сергей Квашин <kvashinsa@mail.ru>
2023-11-01 18:48:20 +0300

    prob

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -68,15 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'foodgram.wsgi.application'
 
-# Database
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
